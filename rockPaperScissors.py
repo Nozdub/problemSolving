eb520248001853ab0ed54dcb86fc3212777a0787
Christian <chriaas@hiof.no>
2022-08-29 00:48:40 +0200

    Added comments to rockPaperScissors.py

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,13 +1,11 @@
-# --------------------------------------------- Rock, paper scissors ---------------------------------------------------#
+# --------------------------------------------- Rock, paper scissors --------------------------------------------------#
 import random
 
 
+# Making main play function with if statements regarding game outcome:
 def play():
     user = input("What's your choice (rock, paper, scissors): ")
     computer = random.choice(["rock", "paper", "scissors"])
-
-
-
 
     if user == computer:
         return f"You both selected {user}, so it's a tie"
@@ -19,6 +17,7 @@ def play():
     return "You lost"
 
 
+# Defining what a win is:
 def is_win(player, opponent):
     # return true if player wins
     # r > s, s > p, p > r
@@ -26,7 +25,7 @@ def is_win(player, opponent):
             or (player == "scissors" and opponent == "paper"):
         return True
 
-
+# Created a loop for the game that plays as long as the answer to continue is something else than no.
 start = input("Do you want to play a game of rock, paper, scissors? (yes/no): ")
 if start == "yes":
     while start:
